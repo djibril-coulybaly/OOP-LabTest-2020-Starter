af7b4d6ffa9be7902f719c2e44a392a6416f3fd7
Djibril Coulybaly <46174096+djibril-coulybaly@users.noreply.github.com>
2020-04-22 11:29:17 +0100

    Declare an ArrayList to hold instances of the Task class

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -1,10 +1,15 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 public class Gantt extends PApplet
-{	
-	
+{
+	//Declare an ArrayList to hold instances of the Task class	
+	ArrayList<Task> gantTasks = new ArrayList<Task>();
+
+
 	public void settings()
 	{
 		size(800, 600);

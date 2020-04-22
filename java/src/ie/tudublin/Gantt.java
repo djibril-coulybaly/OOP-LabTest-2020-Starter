e33da866515b2b4841e15fce236cc19cf0a5f9bd
Djibril Coulybaly <46174096+djibril-coulybaly@users.noreply.github.com>
2020-04-22 11:46:44 +0100

    Implementing loadTasks() and printTasks()

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -3,6 +3,8 @@ package ie.tudublin;
 import java.util.ArrayList;
 
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class Gantt extends PApplet
 {
@@ -14,15 +16,24 @@ public class Gantt extends PApplet
 	{
 		size(800, 600);
 	}
-
+	
+	//Populates the arrayList from the file tasks.csv
 	public void loadTasks()
 	{
-		
+		Table table = loadTable("tasks.csv","header");
+
+        for (TableRow row : table.rows()) {
+            Task ganttAdd = new Task(row);
+            gantTasks.add(ganttAdd);
+        }
 	}
 
+	//Prints the elements of the ArrayList.
 	public void printTasks()
 	{
-		
+		for (Task gantTask: gantTasks) {
+            System.out.println(gantTask);
+        }
 	}
 	
 	public void mousePressed()
@@ -39,6 +50,8 @@ public class Gantt extends PApplet
 	
 	public void setup() 
 	{
+		loadTasks();
+		printTasks();
 	}
 	
 	public void draw()
